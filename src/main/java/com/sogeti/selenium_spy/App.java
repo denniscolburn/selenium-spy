191cf6bab9d990ea7b95486b77b1d6f6dd91bee7
Dennis Colburn <denniscolburn@gmail.com>
2018-10-22 17:40:47 -0400

    Added maven project

---
+++ src/main/java/com/sogeti/selenium_spy/App.java
@@ -0,0 +1,13 @@
+package com.sogeti.selenium_spy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
